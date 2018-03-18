9a40eb8d96ae588832f1094707522f5a952df155
Patrick Flinner <ptflinner@yahoo.com>
2018-03-18 15:22:54 -0700

    More skeleton code

--- cli.js
+++ cli.js
@@ -8,53 +8,15 @@ const flags = yargs.usage('$0: Usage <cmd> [options]')
         command: 'search',
         desc: 'give a search term',
         builder: (yargs) => {
-            return yargs.option('f', {
-                alias: 'films',
-                describe: 'searchs within films'
-            }).option('ppl', {
-                alias: 'people',
-                describe: 'searches within people'
-            }).option('pl',{
-                alias:'planets',
-                describe: 'searches within planets'
-            }).option('sp',{
-                alias:'species',
-                describe:'searches within species'
-            }).option('st',{
-                alias:'starships',
-                describe:'searches within starships'
-            }).option('v',{
-                alias:'vehicles',
-                describe:'searches within vehicles'
+            return yargs.option('id', {
+                alias: 'id',
+                describe: 'search using a id number'
+            }).option('t', {
+                alias: 'term',
+                describe: 'search using a term'
             })
         },
-        handler: (argv) => { app.search(argv.films,argv.people,argv.planets,argv.species,argv.starships,argv.vehicles) }
-    })
-    .command({
-        command: 'id',
-        desc: 'search using an identification number',
-        builder:(yargs)=>{
-            return yargs.option('f', {
-                alias: 'films',
-                describe: 'searchs within films'
-            }).option('ppl', {
-                alias: 'people',
-                describe: 'searches within people'
-            }).option('pl',{
-                alias:'planets',
-                describe: 'searches within planets'
-            }).option('sp',{
-                alias:'species',
-                describe:'searches within species'
-            }).option('st',{
-                alias:'starships',
-                describe:'searches within starships'
-            }).option('v',{
-                alias:'vehicles',
-                describe:'searches within vehicles'
-            })
-        },
-        handler:(argv) =>{ app.searchID(argv.films,argv.people,argv.planets,argv.species,argv.starships,argv.vehicles)}
+        handler: (argv) => { app.main(argv.id,argv.term) }
     })
     .help('help')
     .argv
