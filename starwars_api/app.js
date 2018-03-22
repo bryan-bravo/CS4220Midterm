6346d11284690f8ff55d76306f2535a77b7693a6
Patrick Flinner <ptflinner@yahoo.com>
2018-03-21 17:34:00 -0700

    6 Print statements. Formats the catagory a specific way.2/6 are written' and the others have the beginning part completed

--- starwars_api/app.js
+++ starwars_api/app.js
@@ -50,50 +50,138 @@ const main = (id, term) => {
 const printSelection = (catagory,results) => {
     switch(catagory){
         case 'films':
-            printFilm(results)
-            break;
+            results.forEach(element=>{
+                printFilm(element)
+            })
+        break;
         case 'people':
-            printPeople(results)
-            break;
+            results.forEach(element=>{
+                printPeople(element)
+            })
+        break;
         case 'planets':
-            printPlanets(results)
+            results.forEach(element=>{
+                printPlanets(element)
+            })
             break;
         case 'species':
-            printSpecies(results)
+            results.forEach(element=>{
+                printSpecies(element)
+            })
             break;
         case 'starships':
-            printStarships(results)
-            break;
+            results.forEach(element=>{
+                printStarships(element)
+            })
+        break;
         case 'vehicles':
-            printVehicles(results)
+            results.forEach(element=>{
+                printVehicles(element)
+            })
             break;
         default:
             console.log('Something went wrong with the request')
     }
 }
 
-const printFilm=(results)=>{
-    console.log(results)
+const printFilm=(result)=>{
+    let {title,episode_id,opening_crawl,director,producer,release_date,characters,planets,starships,vehicles,species}=result
+    let information=
+`
+Star Wars 
+Episode ${episode_id}
+${title}
+
+Directed by: ${director}
+Produced by: ${producer}
+
+Released: ${release_date}
+
+------------------------
+Notable Characters:
+${characters}
+
+------------------------
+Planets:
+${planets}
+
+------------------------
+Ships and Vehicles:
+${starships}
+${vehicles}
+
+-----------------------
+Species in the movie:
+${species}
+
+-----------------------
+Extras:
+
+${opening_crawl}
+    `
+    console.log(information)
 }
 
-const printPeople=(results)=>{
-    console.log(results)
+const printPeople=(result)=>{
+    let {name,height,mass,hair_color,skin_color,eye_color,birth_year,gender,homeworld,films,species,vehicles,starships}=result
+    let information=
+    `
+    `
+    console.log(information)
 }
 
-const printPlanets=(results)=>{
-    console.log(results)
+const printPlanets=(result)=>{
+    let {name,rotation_period,orbital_period,diameter,climate,gravity,terrain,surface_water,population,residents,films}=result
+
+    let information=
+    `
+    `
+    console.log(information)
 }
 
-const printSpecies=(results)=>{
-    console.log(results)
+const printSpecies=(result)=>{
+    let {name,classification,designation,average_height,skin_colors,hair_colors,eye_colors,average_lifespan,homeworld,language,people,films}=result
+    
+    let information=
+    `
+    `
+    console.log(information)
 }
 
-const printStarships=(results)=>{
-    console.log(results)
+const printStarships=(result)=>{
+    let {name,model,manufacturer,cost_in_credits,length,max_atmosphering_speed,crew,passengers,cargo_capacity,
+        consumables,hyperdrive_rating,MGLT,starship_class,pilots,films}=result
+    
+        let information=
+        `
+        `
+        console.log(information)
 }
 
-const printVehicles=(results)=>{
-    console.log(results)
+const printVehicles=(result)=>{
+    let {name,model,manufacturer,cost_in_credits,length,max_atmosphering_speed,crew,passengers,cargo_capacity,
+    consumables,vehicle_class,pilots,films}=result
+    
+    let information=
+    `
+    ${name}
+         
+    Model: ${model}
+    Manufacturer: ${manufacturer}
+    Price: ${cost_in_credits} Credits
+    Vehicle Class: ${vehicle_class}
+    -------------------------
+    Specifications:
+    It can support a crew of ${crew} and can carry ${passengers} passengers
+    Atmospheric Speed is ${max_atmosphering_speed} and is ${length} meters long
+    -------------------------
+    Films:
+    ${films}
+    -------------------------
+    Famous Pilots:
+    ${pilots}
+    `
+    console.log(information)
 }
 
 module.exports = {
